f9e757040bc051e55fccf5afe6c692cec82d5ae6
SmogWMS <127910793+SmogWMS@users.noreply.github.com>
2025-10-21 11:33:22 +0200

    Create deploy.ts

---
+++ deploy.ts
@@ -0,0 +1,13 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const TicTacToe = await ethers.getContractFactory("TicTacToe");
+  const tic = await TicTacToe.deploy();
+  await tic.deployed();
+  console.log("TicTacToe deployed to:", tic.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
